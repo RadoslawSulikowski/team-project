43ffab216eaae35dca7eb90d7b2dec0b9bd510a3
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2019-10-24 12:04:20 +0100

    CartController - wstępna implementacja

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createCart")
+    public List createCart() {
+        return new ArrayList<>();
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    public List getAllProducts() {
+        return new ArrayList<>();
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "addProduct")
+    public List addProduct() {
+        return new ArrayList<>();
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public List deleteProduct() {
+        return new ArrayList<>();
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    public List createOrder() {
+        return new ArrayList<>();
+    }
 }
--- src/main/java/com/kodilla/ecommercee/GenericEntity.java
+++ src/main/java/com/kodilla/ecommercee/GenericEntity.java
@@ -1,31 +1,23 @@
 package com.kodilla.ecommercee;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class GenericEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "value")
     private String value;
 
-    public GenericEntity() {
-    }
+    public GenericEntity() {}
+
+    public GenericEntity(String value) { this.value = value; }
+
+    public Long getId() { return id; }
 
     public String getValue() {
         return value;
     }
 
-    public Long getId() {
-
-        return id;
-    }
-
-    public GenericEntity(String value) {
-
-        this.value = value;
-    }
 }
