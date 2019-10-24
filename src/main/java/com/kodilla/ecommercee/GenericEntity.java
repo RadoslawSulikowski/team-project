ab89a07a8210074687b3d07bdee72063054c9384
Venomicon <jangajda9@gmail.com>
2019-10-24 10:04:05 +0200

    All tests passed; Minor publishing corrections

--- src/main/java/com/kodilla/ecommercee/GenericEntity.java
+++ src/main/java/com/kodilla/ecommercee/GenericEntity.java
@@ -1,7 +1,5 @@
 package com.kodilla.ecommercee;
 
-import org.springframework.context.annotation.Bean;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
