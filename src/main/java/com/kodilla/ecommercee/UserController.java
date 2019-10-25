1024d66bf15df7048d7819ced0dd0d5adf0772e8
Patty8919 <pat.maciejewska@gmail.com>
2019-10-25 21:37:23 +0200

    JDP191001-5 - review

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,21 +1,25 @@
 package com.kodilla.ecommercee;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/user")
 public class UserController {
-    public void createUser(UserDto userDto) {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createUser")
+    public String createUser(@RequestBody UserDto userDto) {
+        return "Create new user: " +userDto.getId() +" "+userDto.getUsername();
 
     }
 
-    public void blockUser(UserDto userDto) {
-
+    @RequestMapping(method = RequestMethod.GET, value = "blockUser")
+    public String blockUser(@RequestParam String getUsername) {
+        return getUsername + " blocked";
     }
 
-    public void oneHourAPI(UserDto userDto) {
+    @RequestMapping(method = RequestMethod.PUT, value = "oneHourAPI")
+    public UserDto oneHourAPI(@RequestBody UserDto userDto) {
+        return userDto;
 
     }
 }
--- src/main/java/com/kodilla/ecommercee/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/UserDto.java
@@ -1,19 +1,19 @@
 package com.kodilla.ecommercee;
 
 public class UserDto {
-    private Long id;
+    private Integer id;
     private String username;
     private Integer status;
     private Long userKey;
 
-    public UserDto(Long id, String username, Integer status, Long userKey) {
+    public UserDto(Integer id, String username, Integer status, Long userKey) {
         this.id = id;
         this.username = username;
         this.status = status;
         this.userKey = userKey;
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
