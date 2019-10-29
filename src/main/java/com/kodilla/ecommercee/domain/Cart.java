76be1911b0071bab588781092e697bf7e72262a8
RadoslawSulikowski <radoslaw.sulikowski@gmail.com>
2019-10-29 20:22:35 +0100

    Cart implementation.v1

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -7,10 +7,15 @@ import java.util.List;
 @Entity(name = "CARTS")
 public class Cart {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    /*@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)*/
     private Long cartId;
 
+    /*@OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "product",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)*/
     private List<ProductDto> products = new ArrayList<>();
 
     public Cart() {
