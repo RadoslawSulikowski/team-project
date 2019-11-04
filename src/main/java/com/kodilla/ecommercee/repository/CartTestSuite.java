21ae08901c08444e0c59836b7dff39a1afd6fa43
Patty8919 <pat.maciejewska@gmail.com>
2019-11-05 00:22:11 +0100

    poprawione

--- src/main/java/com/kodilla/ecommercee/repository/CartTestSuite.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartTestSuite.java
@@ -29,31 +29,25 @@ public class CartTestSuite {
         Cart cart2 = new Cart();
         Product product1 = new Product("Kurtka", "Kurtka zimowa", new BigDecimal(100));
         Product product2 = new Product("Płaszcz", "Płaszcz zimowy, męski", new BigDecimal(150));
-        cartRepository.save(cart1);
-        cartRepository.save(cart2);
 
-        //When
-        Optional<Cart> showCart = cartRepository.findById(cart2.getCartId());
-        Optional<Product> showProduct = productRepository.findById(product2.getId());
         List<Product> productsList = new ArrayList<>();
         productsList.add(product1);
         productsList.add(product2);
 
-        cart2.setProducts(productsList);
+        cart1.setProducts(productsList);
+
+        //When
+        cartRepository.save(cart1);
+        cartRepository.save(cart2);
+
+        Optional<Cart> testCart = cartRepository.findById(cart1.getCartId());
+        Optional<Product> testProduct = productRepository.findById(product2.getId());
 
         //Then
-        int sizeBeforeDelete = cartRepository.findAll().size();
-        cartRepository.deleteById(cart2.getCartId());
-        int sizeAfterDelete = cartRepository.findAll().size();
-
-        Assert.assertEquals(sizeBeforeDelete, 2);
-        Assert.assertEquals(sizeAfterDelete, 1);
-        Assert.assertFalse(showCart.isPresent());
-        Assert.assertFalse(showProduct.isPresent());
+        Assert.assertTrue(testCart.isPresent());
+        Assert.assertTrue(testProduct.isPresent());
 
         //CleanUp
-        cartRepository.deleteById(cart1.getCartId());
-        productRepository.deleteById(product1.getId());
         productRepository.deleteById(product2.getId());
 
     }
